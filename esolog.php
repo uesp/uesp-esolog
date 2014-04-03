709b522867831c2fb873dea880ae4a8ce6ad905d
Dave Humphrey <uespnet@gmail.com>
2014-04-02 21:08:36 -0400

    add manual submission form

---
+++ esolog.php
@@ -0,0 +1,203 @@
+<?php
+
+class EsoLogCollector
+{
+	const ELC_OUTPUT_LOG_PATH = "log/";
+	const ELC_INDEX_FILENAME = "log/esolog.index";
+	const ELC_MAX_LOG_FILESIZE = 100000000; 	# Maximum desired size of a log file in bytes
+
+	public $currentLogFilename = "tmp.log";
+	public $currentLogIndex = 1;
+	public $rawLogData = array();
+	
+	
+	public function __construct ()
+	{
+		$this->readIndexFile();
+		$this->currentLogFilename = $this->generateLogFilename($this->currentLogIndex);
+	}
+	
+	
+	public function generateLogFilename($index)
+	{
+		$logFilename = self::ELC_OUTPUT_LOG_PATH . sprintf( 'eso%05d.log', $index);
+		return $logFilename;
+	}
+	
+	
+	public function findNewLogFilename()
+	{
+		$pattern = self::ELC_OUTPUT_LOG_PATH . "eso*.log";
+		$logFiles = glob($pattern);
+		
+		$lastIndex = 0;
+		
+		foreach ($logFiles as $key => $value)
+		{
+			
+			if (preg_match('/eso(.*)\.log/', $value, $matches) === 1)
+			{
+				$index = (int)$matches[1];
+				if ($index > $lastIndex) $lastIndex = $index;
+			}
+		}
+		
+		return $lastIndex + 1;
+	}
+	
+	
+	public function writeIndexFile()
+	{
+		if (file_put_contents(self::ELC_INDEX_FILENAME, (string)$this->currentLogIndex) === FALSE)
+		{
+			error_log("Failed to write the log index file: " . self::ELC_INDEX_FILENAME);
+			return FALSE;
+		}
+		
+		return TRUE;
+	}
+	
+	
+	public function readIndexFile()
+	{
+		$index = file_get_contents(self::ELC_INDEX_FILENAME);
+		
+		if ($index === FALSE)
+		{
+			$this->currentLogIndex = 1;
+			$this->writeIndexFile();
+			return FALSE;
+		}
+		
+		$this->currentLogIndex = (int) $index;
+		if ($this->currentLogIndex < 0) $this->currentLogIndex = 1;
+		
+		return TRUE;
+	}
+	
+	
+	public function reportError ($errorMsg)
+	{
+		print("Error: " . $errorMsg . "\n");
+		error_log("Error: " . $errorMsg);
+		
+		return false;
+	}
+	
+	
+	public function outputQueuedData ($logData)
+	{
+		$result = file_put_contents($this->currentLogFilename, $logData . "\n", FILE_APPEND | LOCK_EX);
+		
+		if ($result == FALSE)
+		{
+			$this->reportError("Failed to append log data to file!");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	public function getExtraData()
+	{
+		$extraData  = "ipAddress{" . $_SERVER["REMOTE_ADDR"] . "}  ";
+		$extraData .= "logTime{" . time() . "}  ";
+		$extraData .= "end{}  ";
+		return $extraData;
+	}
+	
+	
+	public function parseLogDataItem($logData)
+	{
+		$decodedData = base64_decode($logData);
+		return $this->outputQueuedData($decodedData . $this->getExtraData());
+	}
+	
+	
+	public function parseLogData()
+	{
+		$result = true;
+		
+		foreach ($this->rawLogData as $key => $value)
+		{
+			$result = $this->parseLogDataItem($value) and $result;
+		}
+		
+		return $result;
+	}
+	
+	
+	public function createNewLogFile()
+	{
+		$newIndex = $this->currentLogIndex;
+		$numAttempts = 0;
+		
+		while ($numAttempts < 1000)
+		{
+			++$newIndex;
+			++$numAttempts;
+			
+			$filename = $this->generateLogFilename($newIndex);
+			
+			if (!file_exists($filename)) break;
+		}
+		
+		if ($numAttempts >= 1000)
+		{
+			$this->reportError("ERROR: Failed to generate new log filename!");
+			return FALSE;
+		}
+		
+		$this->currentLogIndex = $newIndex;
+		$this->currentLogFilename = $this->generateLogFilename($this->currentLogIndex);
+		$this->writeIndexFile();
+		
+		return TRUE;
+	}
+	
+	
+	public function checkLogSize()
+	{
+		$filesize = filesize($this->currentLogFilename);
+		if ($filesize === FALSE) return FALSE;
+		
+		if ($filesize > self::ELC_MAX_LOG_FILESIZE) $this->createNewLogFile();
+	}
+	
+	
+	public function parseFormInput()
+	{
+		$this->writeHeaders();
+		
+		$this->inputParams = $_REQUEST;
+		
+		if (!array_key_exists('log', $this->inputParams)) return $this->reportError("Failed to find any form input to parse!");
+		$this->rawLogData = $this->inputParams['log'];
+		
+		print("Found log form data with " . count($this->rawLogData) . " elements to parse.");
+		
+		$this->parseLogData();
+		$this->checkLogSize();
+		
+		return true;
+	}
+	
+	
+	public function writeHeaders ()
+	{
+		header("Expires: 0");
+		header("Pragma: no-cache");
+		header("Cache-Control: no-cache, no-store, must-revalidate");
+		header("Pragma: no-cache");
+		header("content-type: text/html");
+	}
+	
+};
+
+
+$g_EsoLogCollector = new EsoLogCollector();
+$g_EsoLogCollector->parseFormInput();
+
+
+?>
---
+++ submit.php
@@ -0,0 +1,337 @@
+<?php 
+
+
+class EsoLogSubmitter
+{
+	
+	const ESOLOG_UPLOAD_PATH = "/home/uesp/www/esolog/log/upload";
+	const ESOLOG_OUTPUT_PATH = "/home/uesp/www/esolog/log/";
+	const ELC_INDEX_FILENAME = "/home/uesp/www/esolog/log/esolog.index";
+	
+	public $fileData = "";
+	public $fileError = 0;
+	public $fileErrorMsg = "";
+	public $fileSize = 0;
+	public $fileTmpName = "";
+	public $fileMoveName = "";
+	public $fileName = "";
+	public $hasFileData = false;
+	public $parsedRecords = 0;
+	public $accountName = "Anonymous";
+	
+	public $currentLogIndex = 1;
+	
+	
+	public function __construct ()
+	{
+	}
+	
+	public function parseFormInput()
+	{
+		$this->writeHeaders();
+		
+		$this->inputParams = $_REQUEST;
+		
+		if (array_key_exists("logfile", $_FILES))
+		{
+			$this->fileName = $_FILES["logfile"]['name'];
+			$this->fileSize = $_FILES["logfile"]['size'];
+			$this->fileError = $_FILES["logfile"]['error'];
+			$this->fileTmpName = $_FILES["logfile"]['tmp_name'];
+			$this->hasFileData = true;
+		}
+		
+		return true;
+	}
+	
+	
+	public function reportError ($errorMsg)
+	{
+		//print("Error: " . $errorMsg . "\n");
+		error_log("Error: " . $errorMsg);
+		$this->fileErrorMsg .= $errorMsg . "<br/>";
+		$this->fileError = 1;
+		
+		return false;
+	}
+	
+	
+	public function outputUpload ()
+	{
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>UESP:uespLog Manual Upload</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" href="submit.css" />
+</head>
+<body>
+
+<table border="0" cellpadding="2" cellspacing="0" id="maintable">
+<tr>
+	<td>
+		<h1>Submitted uespLog for ESO</h1>
+		<br /> &nbsp;
+	</td>
+</tr><tr>
+	<td>
+<?php
+	$output = "";
+	
+	if ($this->fileError != 0)
+	{
+		$output = "<b>Error: Failed to upload file!</b><br />";
+		$output .= $this->fileErrorMsg . "<br />";
+	}
+	else
+	{
+		$output  = "<b>Successfully uploaded file!</b><br />";
+		$output .= "Filename: {$this->fileName}<br />";
+		$output .= "File Size: {$this->fileSize} bytes<br />";
+		$output .= "Local Filename: {$this->fileMoveName}<br />";
+		$output .= "Lua Result: {$this->fileLuaResult}<br />";
+		$output .= "Parsed Records: {$this->parsedRecords}<br />";
+		
+	}
+	
+	print($output);
+?>
+	</td>
+</tr><tr>
+	<td>
+		<a href="submit.php" style="float: right;">Upload another log file...</a>
+	</td>
+	
+</table>
+
+</body>
+</html>
+<?php
+	}
+	
+	public function outputForm ()
+	{
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>UESP:uespLog Manual Upload</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" href="submit.css" />
+</head>
+<body>
+
+<form id="submitform" enctype="multipart/form-data" action="submit.php" method="post">
+
+<table border="0" cellpadding="2" cellspacing="0" id="maintable">
+<tr>
+	<td>
+		<h1>Submit uespLog for ESO</h1>
+		Use this page to manually submit a log file created by uespLog filled with data from ESO. Note that if you are playing on the
+		PC version you can use uespLogMonitor (included with the addon) to automatically upload all logged data.
+		<p />
+		<ul>
+		<li>Choose your uespLog saved variable file (usually under your <em>"\Documents\Elder Scrolls Online\live\SavedVariables\uespLog.lua"</em> or
+			similar directory.</li>
+		<li>Submit file.</li>
+		<li>After submitting you can run the command <em>"/uespreset all"</em> in ESO to clear the log data.</li>
+		<li>It is safe to submit duplicate files or log entries...the log parser can detect and ignore duplicate entries.</li>
+		</ul>
+		<p />
+		Note: Maximum file upload size is 8MB.
+		<br /> &nbsp;
+	</td>
+</tr><tr>
+	<td>
+		<input type="hidden" name="MAX_FILE_SIZE" value="8000000" />
+		<input type="file" name="logfile" value="Choose File..." />
+		<br /> &nbsp;
+	</td>
+</tr><tr>
+	<td>
+		<input type="submit" id="submitbutton" />
+	</td>
+	
+</table>
+
+</form>
+
+</body>
+</html>
+<?php
+		return true;
+	}
+	
+	public function output ()
+	{
+		if ($this->hasFileData)
+		{
+			$this->processUpload();
+			$this->outputUpload();
+		}
+		else
+		{
+			$this->outputForm();
+		}
+	}
+	
+	
+	public function processUpload ()
+	{
+		if ($this->fileError != 0) return false;
+		
+		$destFilename = tempnam(self::ESOLOG_UPLOAD_PATH, "uespLog");
+		$this->fileMoveName = $destFilename;
+		
+		$result = move_uploaded_file ($this->fileTmpName, $destFilename);
+		if (!$result) return $this->reportError("Failed to move temporary upload file!"); 
+		
+		$this->Lua = new Lua();
+		
+		$result = $this->Lua->include($destFilename);
+		$this->fileLuaResult = $result;
+		
+		$this->readIndexFile();
+		$this->parseVarRootLevel($this->Lua->uespLogSavedVars);
+	}
+	
+	
+	public function parseVarRootLevel ($object)
+	{
+		if ($object == null) return $this->reportError("Could not find the root object in the saved LUA variable file!");
+		
+		foreach ($object as $key => $value)
+		{
+			$this->parseVarAccountLevel($key, $value);
+		}
+		
+		return TRUE;
+	}
+	
+	
+	public function parseVarAccountLevel ($parentName, $object)
+	{
+		if ($object == null) return $this->reportError("NULL object found in the {$parentName} section of the saved variable file!");
+		
+		foreach ($object as $key => $value)
+		{
+			$this->parseVarAccountWideLevel($key, $value);
+		}
+		
+		return TRUE;
+	}
+	
+	
+	public function parseVarAccountWideLevel ($parentName, $object)
+	{
+		if ($object == null) return $this->reportError("NULL object found in the {$parentName} section of the saved variable file!");
+		$this->accountName = ltrim($parentName, '@');
+		
+		foreach ($object as $key => $value)
+		{
+			$this->parseVarSectionLevel($key, $value);
+		}
+		
+		return TRUE;
+	}
+	
+	
+	public function parseVarSectionLevel ($parentName, $object)
+	{
+		if ($object == null) return $this->reportError("NULL object found in the {$parentName} section of the saved variable file!");
+		
+		$this->parseVarSectionData("all", $object["all"]);
+		$this->parseVarSectionData("achievements", $object["achievements"]);
+		$this->parseVarSectionData("globals", $object["globals"]);
+	
+		return TRUE;
+	}
+	
+	
+	public function parseVarSectionData ($parentName, $object)
+	{
+		if ($object == null) return $this->reportError("NULL object found in the {$parentName} section of the saved variable file!");
+		
+		$data = $object['data'];
+		if ($object == null) return $this->reportError("Missing 'data' section in the {$parentName} section of the saved variable file!");
+		
+		ksort($data);
+		$logData = "";
+		
+		foreach ($data as $key => $value)
+		{
+			$extraData  = "userName{" . $this->accountName . "}  ";
+			$extraData .= "ipAddress{" . $_SERVER["REMOTE_ADDR"] . "}  ";
+			$extraData .= "logTime{" . time() . "}  ";
+			$extraData .= "end{}  ";
+			
+			$logData .= $value . $extraData . "\n";
+			
+			++$this->parsedRecords;
+		}
+		
+		$this->outputQueuedData($logData);
+		
+		return TRUE;
+	}
+	
+	
+	public function outputQueuedData ($logData)
+	{
+		$this->currentLogFilename = self::ESOLOG_OUTPUT_PATH . sprintf( 'eso%05d.log', $this->currentLogIndex);
+		$result = file_put_contents($this->currentLogFilename, $logData . "\n", FILE_APPEND | LOCK_EX);
+	
+		if ($result == FALSE) return $this->reportError("Failed to append log data to file!");
+		return TRUE;
+	}
+	
+	
+	public function readIndexFile()
+	{
+		$index = file_get_contents(self::ELC_INDEX_FILENAME);
+	
+		if ($index === FALSE)
+		{
+			$this->currentLogIndex = 1;
+			$this->writeIndexFile();
+			return FALSE;
+		}
+	
+		$this->currentLogIndex = (int) $index;
+		if ($this->currentLogIndex < 0) $this->currentLogIndex = 1;
+	
+		return TRUE;
+	}
+	
+	
+	public function writeIndexFile()
+	{
+		if (file_put_contents(self::ELC_INDEX_FILENAME, (string)$this->currentLogIndex) === FALSE)
+		{
+			error_log("Failed to write the log index file: " . self::ELC_INDEX_FILENAME);
+			return FALSE;
+		}
+	
+		return TRUE;
+	}
+	
+	
+	public function writeHeaders ()
+	{
+		header("Expires: 0");
+		header("Pragma: no-cache");
+		header("Cache-Control: no-cache, no-store, must-revalidate");
+		header("Pragma: no-cache");
+		header("content-type: text/html");
+	}
+	
+};
+
+
+$g_EsoLogSubmitter = new EsoLogSubmitter();
+$g_EsoLogSubmitter->parseFormInput();
+$g_EsoLogSubmitter->output();
+
+
+?>
