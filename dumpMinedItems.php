6cb9e8f02aeaa78d1ac4f82fb082d2e253198937
Dave Humphrey <dave@uesp.net>
2014-12-09 17:17:22 -0500

    change to class

--- dumpMinedItems.php
+++ dumpMinedItems.php
@@ -7,151 +7,173 @@
  */
 
 	// Database users, passwords and other secrets
+
 require("/home/uesp/secrets/esolog.secrets");
+require("esoCommon.php");
 
 
-	// Global objects
-$db = null;
-$inputParams = array();
-$SELECT_LIMIT = 1000;
-
-
-function ReportError($errorMsg)
-{
-	print($errorMsg);
-	error_log($errorMsg);
-	return false;
-}
-
-
-function InitDatabase()
-{
-	global $uespEsoLogReadDBHost, $uespEsoLogReadUser, $uespEsoLogReadPW, $uespEsoLogDatabase;
-	global $db;
+class CEsoDumpMinedItems {
 	
-	$db = new mysqli($uespEsoLogReadDBHost, $uespEsoLogReadUser, $uespEsoLogReadPW, $uespEsoLogDatabase);
-	if ($db->connect_error) return ReportError("ERROR: Could not connect to mysql database!");
+	const SELECT_LIMIT = 2000;
+	public $itemId = 0;
+	public $sortField = "";
+	public $db = null;
+	public $outputType = "csv";
+	public $inputParams = array();
 	
-	return true;
-}
-
-
-function ParseInputPrameters()
-{
-	global $inputParams;
-	global $argv;
 	
-	$inputParams['itemid'] = 0;
-	$inputParams['sort'] = "";
-	
-		// Add command line arguments to input parameters for testing
-	if ($argv !== null)
+	public function __construct()
 	{
-		foreach ($argv as $arg)
+		error_reporting(E_ALL);
+		
+		$this->SetInputParams();
+		$this->ParseInputParameters();
+		$this->InitDatabase();
+		
+		$this->OutputHtmlHeader();
+	}
+	
+	
+	public function ReportError($errorMsg)
+	{
+		print($errorMsg);
+		error_log($errorMsg);
+		return false;
+	}
+	
+	
+	public function InitDatabase()
+	{
+		global $uespEsoLogReadDBHost, $uespEsoLogReadUser, $uespEsoLogReadPW, $uespEsoLogDatabase;
+		
+		$this->db = new mysqli($uespEsoLogReadDBHost, $uespEsoLogReadUser, $uespEsoLogReadPW, $uespEsoLogDatabase);
+		if ($this->db->connect_error) return $this->ReportError("ERROR: Could not connect to mysql database!");
+	
+		return true;
+	}
+	
+	
+	private function SetInputParams ()
+	{
+		global $argv;
+		$this->inputParams = $_REQUEST;
+		
+			// Add command line arguments to input parameters for testing
+		if ($argv !== null)
 		{
-			$e = explode("=", $arg);
-	
-			if(count($e) == 2)
-				$inputParams[$e[0]] = $e[1];
-			else
-				$inputParams[$e[0]] = 0;
+			$argIndex = 0;
+				
+			foreach ($argv as $arg)
+			{
+				$argIndex += 1;
+				if ($argIndex <= 1) continue;
+				$e = explode("=", $arg);
+				
+				if(count($e) == 2)
+				{
+					$this->inputParams[$e[0]] = $e[1];
+				}
+				else
+				{
+					$this->inputParams[$e[0]] = 1;
+				}
+			}
 		}
+		
 	}
 	
-	if (array_key_exists('itemid', $_REQUEST))
+	
+	public function ParseInputParameters()
 	{
-		$inputParams['itemid'] = intval($_REQUEST['itemid']);
+		if (array_key_exists('itemid', $this->inputParams)) $this->itemId = intval($this->inputParams['itemid']);
+		
+		if (array_key_exists('sort', $this->inputParams))
+		{
+			$matches = array();
+			$result = preg_match("|^([a-zA-Z0-9]+)|s", $this->inputParams['sort'], $matches);
+			if ($result) $this->sortField = $matches[1];
+		}
+		
+		if (array_key_exists('output', $this->inputParams))
+		{
+			$this->inputParams['output'] = strtolower($this->inputParams['output']);
+			
+			if ($this->inputParams['output'] == "csv") 
+				$this->outputType = "csv";
+			elseif ($this->inputParams['output'] == "html")
+				$this->outputType = "html";
+			elseif ($this->inputParams['output'] == "wiki")
+				$this->outputType = "wiki";
+			
+		}
+		
+		return true;
 	}
 	
-	if (array_key_exists('sort', $_REQUEST))
+	
+	public function OutputHtmlHeader()
 	{
-		$matches = array();
-		$result = preg_match("|^([a-zA-Z0-9]+)|s", $_REQUEST['sort'], $matches);
-		if ($result) $inputParams['sort'] = $matches[1];
+		header("Expires: 0");
+		header("Pragma: no-cache");
+		header("Cache-Control: no-cache, no-store, must-revalidate");
+		header("Pragma: no-cache");
+		header("content-type: text/plain");
 	}
 	
-	return true;
-}
-
-
-function OutputHtmlHeader()
-{
-	header("Expires: 0");
-	header("Pragma: no-cache");
-	header("Cache-Control: no-cache, no-store, must-revalidate");
-	header("Pragma: no-cache");
-	header("content-type: text/plain");
-}
-
-
-function InitProgram()
-{
-	error_reporting(E_ALL);
-	OutputHtmlHeader();
 	
-	if (!InitDatabase()) return false;
-	if (!ParseInputPrameters()) return false;
-	
-	return true;
-}
-
-
-function LoadRecords()
-{
-	global $db;
-	global $inputParams;
-	global $SELECT_LIMIT;
-	
-	$itemId = $inputParams['itemid'];
-	$sort = $inputParams['sort'];
-
-	if ($itemId <= 0) return ReportError("ERROR: No itemid specified!");
-	
-	$query = "SELECT * FROM minedItem WHERE itemId=$itemId";
-	if ($sort != "") $query .= " ORDER BY $sort";
-	$query .= " LIMIT $SELECT_LIMIT";
-	
-	$result = $db->query($query);
-	if (!$result) return false;
-	
-	$records = array();
-	if ($result->num_rows === 0) return $records;
-	
-	$result->data_seek(0);
-	
-	while (($row = $result->fetch_assoc()))
+	public function LoadRecords()
 	{
-		$records[] = $row;
+		$itemId = $this->itemId;
+		$sort = $this->sortField;
+		
+		if ($itemId <= 0) return $this->ReportError("ERROR: No itemid specified!");
+		
+		$query = "SELECT * FROM minedItem WHERE itemId=$itemId";
+		if ($sort != "") $query .= " ORDER BY $sort";
+		$query .= " LIMIT " . self::SELECT_LIMIT;
+		
+		$result = $this->db->query($query);
+		if (!$result) return $this->ReportError("ERROR: Database query error!");
+		
+		$records = array();
+		if ($result->num_rows === 0) return $records;
+		$result->data_seek(0);
+		
+		while (($row = $result->fetch_assoc()))
+		{
+			$records[] = $row;
+		}
+		
+		return $records;
 	}
 	
-	return $records;
-}
-
-
-
-function OutputRecords($records)
-{
-	print("id, itemid, level, quality, value, intlevel, intsubtype, weaponPower, armorRating\n");
 	
-	foreach ($records as $key => $value)
+	public function OutputRecords($records)
 	{
-		print("${value['id']}, ${value['itemId']}, ${value['level']}, ${value['quality']}, ${value['value']}, ${value['internalLevel']}, ${value['internalSubtype']}, ${value['weaponPower']}, ${value['armorRating']},\n");
+		print("id, itemid, level, quality, value, intlevel, intsubtype, weaponPower, armorRating\n");
+		
+		foreach ($records as $key => $value)
+		{
+			print("${value['id']}, ${value['itemId']}, ${value['level']}, ${value['quality']}, ${value['value']}, ${value['internalLevel']}, ${value['internalSubtype']}, ${value['weaponPower']}, ${value['armorRating']},\n");
+		}
+		
 	}
 	
-}
-
-
-// 	Begin main program
-function DoMain()
-{
-	if (!InitProgram()) return false;
-	$records = LoadRecords();
-	if (!$records) return false;
 	
-	OutputRecords($records);
-}
+	public function Output()
+	{
+		$records = $this->LoadRecords();
+		if (!$records) return false;
+		
+		$this->OutputRecords($records);
+	}
+	
+	
+};
 
 
-DoMain();
+$g_EsoDumpMinedItems = new CEsoDumpMinedItems();
+$g_EsoDumpMinedItems->Output();
+
 
 ?>
