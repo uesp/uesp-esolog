fc57043746c57f6e50d4921c452c73a5b589ea12
Dave Humphrey <uespnet@gmail.com>
2015-10-01 12:35:00 -0400

    move old style links to another table

--- cleanItems.php
+++ cleanItems.php
@@ -1,5 +1,6 @@
 <?php
 
+$OLD_ITEM_TABLE = "item16";		// Place to move items with only 20 data fields
 $TABLE_SUFFIX = "";
 
 if (php_sapi_name() != "cli") die("Can only be run from command line!");
@@ -9,6 +10,13 @@ require("/home/uesp/secrets/esolog.secrets");
 
 $db = new mysqli($uespEsoLogWriteDBHost, $uespEsoLogWriteUser, $uespEsoLogWritePW, $uespEsoLogDatabase);
 if ($db->connect_error) exit("Could not connect to mysql database!");
+
+if ($OLD_ITEM_TABLE != "")
+{
+	$query = "CREATE TABLE IF NOT EXISTS $OLD_ITEM_TABLE LIKE item$TABLE_SUFFIX;";
+	$result = $db->query($query);
+	if (!$result) exit("ERROR: Database query error creating  $OLD_ITEM_TABLE table!\n" . $db->error);
+}
 
 $query = "SELECT * FROM item$TABLE_SUFFIX WHERE name LIKE '|%';";
 $itemResult = $db->query($query);
@@ -45,11 +53,12 @@ $itemResult = $db->query($query);
 if (!$itemResult) exit("ERROR: Database query error finding links in item names!\n" . $db->error);
 $itemResult->data_seek(0);
 $itemCount = 0;
+$moveOldCount = 0;
 
 while (($item = $itemResult->fetch_assoc()))
 {
 	$itemLink = $item['link'];
-	$result = preg_match('/\|H[A-Za-z0-9]*\:item(\:[0-9]*)*\|h(?P<name>[^|\^]*)(?P<nameCode>.*?)\|h/', $itemLink, $matches);
+	$result = preg_match('/\|H[A-Za-z0-9]*\:item(?P<data>(\:[0-9]*)*)\|h(?P<name>[^|\^]*)(?P<nameCode>.*?)\|h/', $itemLink, $matches);
 	
 	if ($result == 0)
 	{
@@ -57,23 +66,40 @@ while (($item = $itemResult->fetch_assoc()))
 		continue;
 	}
 	
+	$colonCount = substr_count($matches['data'], ':');
+	if ($colonCount < 20 || $colonCount > 21) print("\t$colonCount : {$matches['data']}\n");
+	
 	$itemName = $matches['name'];
 	$oldName = $item['name'];
 	if ($matches['nameCode'] != '' && $matches['nameCode'][0] != '^') print("\tNamecode '{$matches['nameCode']}' for '$oldName'!\n");
-	
-	if ($itemName == $oldName) continue;
-	$safeName = $db->real_escape_string($itemName);
 	$id = $item['id'];
 	
-	print("\tFixing item name '$oldName' to '$itemName\n");
+	if ($itemName != $oldName)
+	{
+		$safeName = $db->real_escape_string($itemName);
+		print("\tFixing item name '$oldName' to '$itemName\n");
+		
+		$query = "UPDATE item$TABLE_SUFFIX SET name='$safeName' WHERE id=$id;";
+		$writeResult = $db->query($query);
+		if (!$writeResult) exit("ERROR: Database query error saving item!\n" . $db->error);
+		$itemCount++;
+	}
 	
-	$query = "UPDATE item$TABLE_SUFFIX SET name='$safeName' WHERE id=$id;";
-	$writeResult = $db->query($query);
-	if (!$writeResult) exit("ERROR: Database query error saving item!\n" . $db->error);
-	$itemCount++;
+	if($OLD_ITEM_TABLE != '' && $colonCount == 20)
+	{
+		$query = "INSERT IGNORE $OLD_ITEM_TABLE SELECT * FROM item$TABLE_SUFFIX WHERE id=$id;";
+		$writeResult = $db->query($query);
+		if (!$writeResult) exit("ERROR: Database query error copying item to old table!\n" . $db->error);
+		
+		$query = "DELETE FROM item$TABLE_SUFFIX WHERE id=$id;";
+		$writeResult = $db->query($query);
+		if (!$writeResult) exit("ERROR: Database query error deleting item!\n" . $db->error);
+		$moveOldCount++;
+	}
 }
 
 print("Fixed $itemCount items names.\n");
+print("Moved $moveOldCount old items.\n");
 
 $query = "SELECT name,count(*) as c,link FROM item WHERE link != '' GROUP BY link HAVING c>1;";
 $itemResult = $db->query($query);
