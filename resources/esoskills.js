074a7e5fd4f163879182be7f5d88af4d7998d253
Dave Humphrey <dave@uesp.net>
2016-03-25 14:34:40 -0400

    basic work on skill tree

---
+++ resources/esoskills.js
@@ -0,0 +1,56 @@
+function EsoSkillShowSkillLine(skillLine)
+{
+	var id = skillLine.replace(/[ '"]/g, '_');
+	
+	console.log("'"+skillLine+"'", "'"+id+"'");
+	
+	$(".esovsSkillContentBlock:visible").hide();
+	$("#" + id).show();
+}
+
+function OnEsoSkillTypeTitleClick(event)
+{
+	var currentSkillType = $(".esovsSkillTypeTitle.esovsSkillLineTitleHighlight");
+	var currentSkillLine = $(".esovsSkillLineTitle.esovsSkillTypeTitleHighlight");
+	
+	if ($(this)[0] == currentSkillType[0]) return;
+	
+	$(".esovsSkillType:visible").slideUp();
+	currentSkillType.removeClass("esovsTitleHighlight");
+	currentSkillLine.removeClass("esovsTitleHighlight");
+	
+	$(this).next(".esovsSkillType").slideDown();
+	$(this).addClass("esovsSkillLineTitleHighlight");
+	
+	var firstSkillLine = $(this).next(".esovsSkillType").children(".esovsSkillLineTitle").first();
+	firstSkillLine.addClass("esovsSkillTypeTitleHighlight");
+	
+	var skillType = $(this).text();
+	var skillLine = firstSkillLine.text();
+	EsoSkillShowSkillLine(skillLine);
+}
+
+
+function OnEsoSkillLineTitleClick(event)
+{
+	var currentSkillLine = $(".esovsSkillLineTitle.esovsSkillTypeTitleHighlight");
+	
+	if ($(this)[0] == currentSkillLine[0]) return;
+	currentSkillLine.removeClass("esovsSkillTypeTitleHighlight");
+	
+	$(this).addClass("esovsSkillTypeTitleHighlight");
+	
+	var skillLine = $(this).text();
+	
+	EsoSkillShowSkillLine(skillLine);
+}
+
+
+function esovsOnDocReady()
+{
+	$('.esovsSkillTypeTitle').click(OnEsoSkillTypeTitleClick);
+	$('.esovsSkillLineTitle').click(OnEsoSkillLineTitleClick);
+}
+
+
+$( document ).ready(esovsOnDocReady);	
