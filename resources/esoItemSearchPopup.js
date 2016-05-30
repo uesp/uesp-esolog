36edd31745a78f6a7dd5a8e1032c7d2c414f3bb2
Dave Humphrey <dave@uesp.net>
2016-05-30 09:14:12 -0400

    work on item search popup

--- resources/esoItemSearchPopup.js
+++ resources/esoItemSearchPopup.js
@@ -7,6 +7,7 @@ UESP.esoItemSearchPopup = null;
 	/* Class Constructor */
 UESP.EsoItemSearchPopup = function () 
 {
+	this.queryURL = "http://esolog.uesp.net/esoItemSearchPopup.php";
 	this.rootElement = this.create();
 };
 
@@ -24,6 +25,15 @@ UESP.EsoItemSearchPopup.prototype.create = function()
 	$("#esoispCloseButton").click(function() { self.onClose(); });
 	$("#esoispSearchButton").click(function() { self.onSearch(); });
 	
+	$("#esoispInputText").keyup(function (e) {
+	    if (e.keyCode == 13) {
+	    	self.onSearch();
+	    	this.blur();
+	    }
+	});
+	
+	$("#esoispResults").scroll(function(e) { self.onResultsScroll(e); })
+	
 	return rootElement;
 }
 
@@ -31,8 +41,9 @@ UESP.EsoItemSearchPopup.prototype.create = function()
 UESP.EsoItemSearchPopup.prototype.getPopupRootText = function()
 {
 	return "" +
-		"<div id='esoispCloseButton'></div>" +
+		"<div id='esoispCloseButton'>x</div>" +
 		"<div id='esoispInputs'>" + 
+		"	<p>" +
 		"	Text <input id='esoispInputText' type='text' name='text' value=''>" +
 		"	<button id='esoispSearchButton' class='esoispButton'>Search...</button>" +
 		"</div>" +
@@ -60,11 +71,93 @@ UESP.EsoItemSearchPopup.prototype.onClose = function()
 };
 
 
+UESP.EsoItemSearchPopup.prototype.getSearchQueryParam = function()
+{
+	var queryParams = {};
+	
+	var text = $("#esoispInputText").val().trim();
+	
+	if (text != "") queryParams['text'] = text;
+	
+	return queryParams;
+}
+
+
 UESP.EsoItemSearchPopup.prototype.onSearch = function() 
 {
+	var queryParams = this.getSearchQueryParam();
 	
+	if (this.sendSearchQuery(queryParams))
+	{
+		$("#esoispResults").text("Searching...");
+	}
 };
 
+
+UESP.EsoItemSearchPopup.prototype.sendSearchQuery = function(queryParams)
+{
+	var self = this;
+	
+	if ($.isEmptyObject(queryParams)) return false;
+	
+	$.ajax(this.queryURL, {
+			data: queryParams,
+		}).
+		done(function(data, status, xhr) { self.onSearchResults(data, status, xhr); }).
+		fail(function(xhr, status, errorMsg) { self.onSearchError(xhr, status, errorMsg); });
+	
+	return true;
+}
+
+
+UESP.EsoItemSearchPopup.prototype.onSearchError = function(xhr, status, errorMsg)
+{
+	$("#esoispResults").text("Error: " + errorMsg);
+}
+
+
+UESP.EsoItemSearchPopup.prototype.onSearchResults = function(data, status, xhr)
+{
+	data.sort(function(a, b) { return a.name.localeCompare(b.name); });
+	
+	this.displaySearchResults(data);
+}
+
+
+UESP.EsoItemSearchPopup.prototype.onResultsScroll = function(e) 
+{
+	//console.log("scroll");
+	//e.preventDefault();
+	//e.stopPropagation();
+}
+
+
+UESP.EsoItemSearchPopup.prototype.displaySearchResults = function(itemData)
+{
+	var resultsElement = $("#esoispResults");
+	var newResults = "";
+	
+	for (var i = 0; i < itemData.length; ++i)
+	{
+		newResults += this.createSearchResult(itemData[i]);
+	}
+		
+	resultsElement.html(newResults);
+}
+
+
+UESP.EsoItemSearchPopup.prototype.createSearchResult = function(itemData)
+{
+	var resultHtml;
+	
+	resultHtml = 	"<div class='esoispResultRow'>" +
+						"" + itemData.name +
+					"</div>";
+	
+	
+	
+	return resultHtml;
+}
 
 
 	/* Main function entrance */
