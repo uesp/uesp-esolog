b80208dacce0204e13ee2a898fba0eeba8174122
Dave Humphrey <dave@uesp.net>
2017-02-14 20:34:57 -0500

    work on alchemy viewer

--- viewPotions.php
+++ viewPotions.php
@@ -64,9 +64,9 @@ class CEsoViewPotions
 	}
 	
 	
-	public function GetRegeantsHtml()
+	public function GetReagentsHtml()
 	{
-		global $ESO_REAGENT_DATA, $ESO_POTION_DATA, $ESO_UNKNOWN_POTION_EFFECT;
+		global $ESO_REAGENT_DATA, $ESO_POTIONEFFECT_DATA, $ESO_UNKNOWN_POTION_EFFECT;
 		
 		$output = "";
 		
@@ -74,37 +74,102 @@ class CEsoViewPotions
 		{
 			$iconUrl = $reagent['icon'];
 			$name = $reagent['name'];
+			$effectIndex = $reagent['id'];
 			
-			$trait1 = $ESO_POTION_DATA[$reagent['effects'][0]] ?: $ESO_UNKNOWN_POTION_EFFECT;
-			$trait2 = $ESO_POTION_DATA[$reagent['effects'][1]] ?: $ESO_UNKNOWN_POTION_EFFECT;
-			$trait3 = $ESO_POTION_DATA[$reagent['effects'][2]] ?: $ESO_UNKNOWN_POTION_EFFECT;
-			$trait4 = $ESO_POTION_DATA[$reagent['effects'][3]] ?: $ESO_UNKNOWN_POTION_EFFECT;
+			$trait1 = $ESO_POTIONEFFECT_DATA[$reagent['effects'][0]] ?: $ESO_UNKNOWN_POTION_EFFECT;
+			$trait2 = $ESO_POTIONEFFECT_DATA[$reagent['effects'][1]] ?: $ESO_UNKNOWN_POTION_EFFECT;
+			$trait3 = $ESO_POTIONEFFECT_DATA[$reagent['effects'][2]] ?: $ESO_UNKNOWN_POTION_EFFECT;
+			$trait4 = $ESO_POTIONEFFECT_DATA[$reagent['effects'][3]] ?: $ESO_UNKNOWN_POTION_EFFECT;
 			
 			$traitIcon1 = $trait1['icon'];
 			$traitIcon2 = $trait2['icon'];
 			$traitIcon3 = $trait3['icon'];
 			$traitIcon4 = $trait4['icon'];
 			
-			$output .= "<div class='esopdReagent'>";
-			$output .= "<img class=esopdReagentIcon' src='$iconUrl'> <div class='esopdReagentName'>$name</div>";
-			$output .= "<div class='esopdTraits'>";
-			$output .= "<img src='$traitIcon1'>";
-			$output .= "<img src='$traitIcon2'>";
-			$output .= "<img src='$traitIcon3'>";
-			$output .= "<img src='$traitIcon4'>";
+			$traitText1 = $trait1['name'];
+			$traitText2 = $trait2['name'];
+			$traitText3 = $trait3['name'];
+			$traitText4 = $trait4['name'];
+			
+			$output .= "<div class=\"esopdReagent\" reagent=\"$name\" effectindex=\"$effectIndex\">";
+			$output .= "<img class=\"esopdReagentIcon\" src=\"$iconUrl\"><br /><div class=\"esopdReagentName\">$name</div><br />";
+			$output .= "<div class=\"esopdTraits\">";
+			$output .= "<img src='$traitIcon1' title='$traitText1'>";
+			$output .= "<img src='$traitIcon2' title='$traitText2'>";
+			$output .= "<img src='$traitIcon3' title='$traitText3'>";
+			$output .= "<img src='$traitIcon4' title='$traitText4'>";
 			$output .= "</div>";
 			$output .= "</div>";
 		}
 		
+		return $output;
+	}
+	
+	
+	public function GetEffectsHtml()
+	{
+		global $ESO_POTIONEFFECT_DATA;
+		
+		$output = "";
+		$sortedEffects = $ESO_POTIONEFFECT_DATA;
+		usort($sortedEffects, EsoSortPotionDataName);
+				
+		foreach ($sortedEffects as $effect)
+		{
+			$effectIndex = $effect['id'];
+			$icon = $effect['icon'];
+			$name = $effect['name'];
+			$isPositive = $effect['isPositive'];
+			
+			$extraClass = "esopdEffectPositive";
+			if (!$isPositive) $extraClass = "esopdEffectNegative";
+			
+			$output .= "<div class=\"esopdEffect $extraClass\" effectindex=\"$effectIndex\">";
+			$output .= "<img src='$icon'> $name";
+			$output .= "</div>";
+		}		
 		
 		return $output;
+	}
+	
+	
+	public function GetReagentJson()
+	{
+		global $ESO_REAGENT_DATA;
+		
+		$data = array();
+		
+		foreach ($ESO_REAGENT_DATA as $reagent)
+		{
+			$data[$reagent['name']] = $reagent;
+		}
+		
+		return json_encode($data);
+	}
+	
+	
+	public function GetEffectJson()
+	{
+		global $ESO_POTIONEFFECT_DATA;
+		return json_encode($ESO_POTIONEFFECT_DATA);
+	}
+	
+	
+	public function GetUnknownEffectJson()
+	{
+		global $ESO_UNKNOWN_POTION_EFFECT;
+		return json_encode($ESO_UNKNOWN_POTION_EFFECT);
 	}
 	
 	
 	public function CreateOutputHtml()
 	{
 		$replacePairs = array(
-				'{reagents}' => $this->GetRegeantsHtml(),
+				'{reagents}' => $this->GetReagentsHtml(),
+				'{effects}' => $this->GetEffectsHtml(),
+				'{reagentJS}' => $this->GetReagentJson(),
+				'{effectJS}' => $this->GetEffectJson(),
+				'{unknownEffectJS}' => $this->GetUnknownEffectJson(),
 		);
 	
 		$output = strtr($this->htmlTemplate, $replacePairs);
@@ -126,6 +191,12 @@ class CEsoViewPotions
 	}
 	
 };
+
+
+function EsoSortPotionDataName($a, $b)
+{
+	return strcmp($a['name'], $b['name']);
+}
 
 
 $viewPotions = new CEsoViewPotions();
