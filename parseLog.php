fc2e47ed2e3bd51f3160a5da339d205541d72e15
Dave Humphrey <dave@uesp.net>
2019-03-05 14:37:38 -0500

    update runebox ids

--- parseLog.php
+++ parseLog.php
@@ -92,8 +92,8 @@ class EsoLogParser
 		/* Ignore any guild sales earlier than this timestamp */
 	const START_GUILDSALESDATA_TIMESTAMP = 0;
 	
-	const MINEITEM_TABLESUFFIX = "21pts";
-	const SKILLS_TABLESUFFIX   = "21";
+	const MINEITEM_TABLESUFFIX = "";
+	const SKILLS_TABLESUFFIX   = "";
 	
 		/* Parse or skip certain types of log entries. */
 	const ONLY_PARSE_SALES = false;
