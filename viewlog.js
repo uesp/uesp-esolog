102c636e06c87adb27451c38015410e6e8f3fd3a
Dave Humphrey <dave@uesp.net>
2019-04-30 12:45:37 -0400

    tweak search for npc loots

---
+++ viewlog.js
@@ -0,0 +1,18 @@
+function OnEsoLogViewSearchRowClick()
+{
+	var link = $(this).find("a").first();
+	
+	if (link.length == 0) return;
+	
+	var url = link.attr("href");
+	window.location = url;	
+}
+
+
+window.esologViewerOnDocReady = function ()
+{
+	$(".esologSearchRow").click(OnEsoLogViewSearchRowClick)
+}
+
+
+$( document ).ready(esologViewerOnDocReady);
