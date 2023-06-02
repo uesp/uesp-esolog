6709e031e3b33b538cb289a5766e664720b12959
Dave Humphrey <dave@uesp.net>
2023-06-02 11:37:54 -0400

    remove temporary file

--- fixitems.lua
+++
@@ -1,5 +0,0 @@
-uespLog.MineSingleItemSafe_FinishCallback = uespLog.StartNextMineTest
-function uespminetest1()
-	uespLog.MineItemSingle(99767, 1, 2)
-	uespLog.Msg('Done fixing mined items...')
-end
